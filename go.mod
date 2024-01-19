c4cc269c94cfb9ce417409cb2cdb13eadea8ad1a
GUO YANKE <hi@guoyk.xyz>
2024-01-19 14:20:49 +0800

    feat: allow adding more units from env, #9

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/yankeguo/minit
 go 1.21
 
 require (
-	github.com/yankeguo/rg v1.1.0
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/net v0.19.0
-	golang.org/x/sys v0.15.0
+	github.com/yankeguo/rg v1.1.0
+	golang.org/x/net v0.20.0
+	golang.org/x/sys v0.16.0
 	golang.org/x/text v0.14.0
 	gopkg.in/yaml.v3 v3.0.1
 )
--- pkg/munit/load.go
+++ pkg/munit/load.go
@@ -56,6 +56,138 @@ func LoadArgs(args []string) (unit Unit, ok bool, err error) {
 	return
 }
 
+const (
+	UnitPrefix = "MINIT_UNIT_"
+)
+
+// DetectEnvInfixes detects infixes from environment variables
+func DetectEnvInfixes() (infixes []string) {
+	_infixes := map[string]struct{}{}
+
+	for _, item := range os.Environ() {
+		splits := strings.SplitN(item, "=", 2)
+		if len(splits) != 2 {
+			continue
+		}
+
+		key := splits[0]
+		if !strings.HasPrefix(key, UnitPrefix) {
+			continue
+		}
+		key = strings.TrimPrefix(key, UnitPrefix)
+		if strings.HasSuffix(key, "_COMMAND") {
+			key = strings.TrimSuffix(key, "_COMMAND")
+			if key == "" {
+				continue
+			}
+			_infixes[key] = struct{}{}
+		} else if strings.HasSuffix(key, "_FILES") {
+			key = strings.TrimSuffix(key, "_FILES")
+			if key == "" {
+				continue
+			}
+			if os.Getenv(UnitPrefix+key+"_KIND") == KindRender {
+				_infixes[key] = struct{}{}
+			}
+		}
+	}
+
+	for infix := range _infixes {
+		infixes = append(infixes, infix)
+	}
+
+	return
+}
+
+// LoadFromEnvWithInfix loads unit from environment variables with infix
+// e.g. MINIT_MAIN_KIND, MINIT_MAIN_NAME, MINIT_MAIN_COMMAND
+func LoadFromEnvWithInfix(infix string) (unit Unit, ok bool, err error) {
+	// kind
+	unit.Kind = os.Getenv(UnitPrefix + infix + "_KIND")
+
+	if unit.Kind == "" {
+		unit.Kind = KindDaemon
+	}
+
+	switch unit.Kind {
+	case KindDaemon, KindOnce, KindCron, KindRender:
+	default:
+		err = errors.New("unsupported $" + UnitPrefix + infix + "_KIND: " + unit.Kind)
+		return
+	}
+
+	// name, group, count
+	unit.Name = os.Getenv(UnitPrefix + infix + "_NAME")
+	if unit.Name == "" {
+		unit.Name = "env-" + strings.ToLower(infix)
+	}
+	unit.Group = os.Getenv(UnitPrefix + infix + "_GROUP")
+	unit.Count, _ = strconv.Atoi(os.Getenv("MINIT_" + infix + "_COUNT"))
+
+	// command, dir, shell, charset
+	switch unit.Kind {
+	case KindDaemon, KindOnce, KindCron:
+		cmd := os.Getenv(UnitPrefix + infix + "_COMMAND")
+
+		if unit.Command, err = shellquote.Split(cmd); err != nil {
+			return
+		}
+
+		if len(unit.Command) == 0 {
+			err = errors.New("missing environment variable $MINIT_" + infix + "_COMMAND")
+			return
+		}
+
+		unit.Dir = os.Getenv(UnitPrefix + infix + "_DIR")
+		unit.Shell = os.Getenv(UnitPrefix + infix + "_SHELL")
+		unit.Charset = os.Getenv(UnitPrefix + infix + "_CHARSET")
+
+		for _, item := range strings.Split(os.Getenv(UnitPrefix+infix+"_ENV"), ";") {
+			item = strings.TrimSpace(item)
+			splits := strings.SplitN(item, "=", 2)
+			if len(splits) == 2 {
+				if unit.Env == nil {
+					unit.Env = make(map[string]string)
+				}
+				unit.Env[splits[0]] = splits[1]
+			}
+		}
+	}
+
+	// cron, immediate
+	if unit.Kind == KindCron {
+		unit.Cron = os.Getenv(UnitPrefix + infix + "_CRON")
+
+		if unit.Cron == "" {
+			err = errors.New("missing environment variable $" + UnitPrefix + infix + "_CRON while $" + UnitPrefix + infix + "_KIND is 'cron'")
+			return
+		}
+
+		unit.Immediate, _ = strconv.ParseBool(os.Getenv(UnitPrefix + infix + "_IMMEDIATE"))
+	}
+
+	// raw, files
+	if unit.Kind == KindRender {
+		unit.Raw, _ = strconv.ParseBool(os.Getenv(UnitPrefix + infix + "_RAW"))
+
+		for _, item := range strings.Split(os.Getenv(UnitPrefix+infix+"_FILES"), ";") {
+			item = strings.TrimSpace(item)
+			if item != "" {
+				unit.Files = append(unit.Files, item)
+			}
+		}
+
+		if len(unit.Files) == 0 {
+			err = errors.New("missing environment variable $" + UnitPrefix + infix + "_FILES while $" + UnitPrefix + infix + "_KIND is 'render'")
+			return
+		}
+	}
+
+	ok = true
+
+	return
+}
+
 func LoadEnv() (unit Unit, ok bool, err error) {
 	cmd := strings.TrimSpace(os.Getenv("MINIT_MAIN"))
 	if cmd == "" {
